bd42348d87f5f7453565bcd677cb87ab77797af4
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-04-05 18:54:16 -0600

    refactor Stack class: improve print method formatting and ensure pop method functionality

--- stack/main.py
+++ stack/main.py
@@ -12,9 +12,11 @@ class Stack:
 
     def print(self):
         temp = self.top
+        print('\n---')
         while temp:
             print(temp.value)
             temp = temp.next
+        print('---\n')
 
     def push(self, value):
         new_node = Node(value)
@@ -25,6 +27,17 @@ class Stack:
         self.height += 1
         return new_node
 
+    def pop(self):
+        if self.height == 0:
+            return False
+
+        old = self.top
+        self.top = old.next
+        old.next = None
+
+        self.height -= 1
+        return True
+
 
 # push method
 # stack = Stack(1)
@@ -33,3 +46,12 @@ class Stack:
 # stack.push(4)
 # stack.push(100)
 # stack.print()
+stack = Stack(1)
+stack.push(2)
+stack.push(3)
+stack.push(4)
+stack.push(5)
+stack.print()
+print(stack.pop())
+print(stack.pop())
+stack.print()
