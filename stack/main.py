255cdd76f06f9df2218a170d27aa14c89d7eeedd
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-04-05 18:49:38 -0600

    refactor Stack class initialization and enhance push method functionality

--- stack/main.py
+++ stack/main.py
@@ -6,6 +6,30 @@ class Node:
 
 class Stack:
     def __init__(self, value):
-        newNode = Node(value)
-        self.top = newNode
+        new_node = Node(value)
+        self.top = new_node
         self.height = 1
+
+    def print(self):
+        temp = self.top
+        while temp:
+            print(temp.value)
+            temp = temp.next
+
+    def push(self, value):
+        new_node = Node(value)
+        if self.height == 0:
+            self.top = new_node
+
+        new_node.next, self.top = self.top, new_node
+        self.height += 1
+        return new_node
+
+
+# push method
+# stack = Stack(1)
+# stack.push(2)
+# stack.push(3)
+# stack.push(4)
+# stack.push(100)
+# stack.print()
