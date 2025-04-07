9712ac6f3e5cd4e78b421ec8c417fbf6798ed8ad
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-04-07 13:33:15 -0600

    implement contains method for BinarySearchTree class and update test cases

--- tree/main.py
+++ tree/main.py
@@ -34,12 +34,45 @@ class BinarySearchTree:
                     return True
                 temp = temp.right
 
+    def contains(self, value):
+        if not self.root:
+            return False
 
+        if self.root.value == value:
+            return True
+
+        temp = self.root
+
+        while True:
+            if temp.value == value:
+                return True
+
+            if temp.value > value:
+                if not temp.left:
+                    return False
+                temp = temp.left
+            else:
+                if not temp.right:
+                    return False
+                temp = temp.right
+
+
+# Tree insert method
+# tree = BinarySearchTree()
+# tree.insert(2)
+# tree.insert(3)
+# tree.insert(9)
+# tree.insert(5)
+# tree.insert(7)
+# tree.insert(6)
+# print(tree.root.value)
+# print(tree.root.left.value)
+# print(tree.root.right.value)
+
+# Tree Contains method
 tree = BinarySearchTree()
-tree.insert(2)
-tree.insert(3)
-tree.insert(1)
-
-print(tree.root.value)
-print(tree.root.left.value)
-print(tree.root.right.value)
+print(tree.contains(4))  # returns False
+print(tree.contains(9))  # returns True
+print(tree.contains(2))  # returns True
+print(tree.contains(6))  # returns True
+print(tree.contains(0))  # returns False
