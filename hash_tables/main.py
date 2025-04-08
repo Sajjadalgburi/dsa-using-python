df08ad4e89b9d42dd816d2a05b1bbad6fa04efa9
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-04-07 21:29:05 -0600

    implement get_item method for HashTable class

--- hash_tables/main.py
+++ hash_tables/main.py
@@ -21,9 +21,27 @@ class HashTable:
             self.data_map[index] = []
         self.data_map[index].append([key, value])
 
+    def get_item(self, key):
+        index = self.__hash(key)
 
+        if self.data_map[index] is not None:
+            for i in range(len(self.data_map[index])):
+                if self.data_map[index][i][0] == key:
+                    return self.data_map[index][i][1]
+
+        return None
+
+
+# Hash Table set method
 # my_hash = HashTable()
 # my_hash.set_item('bolts', 1400)
 # my_hash.set_item('washers', 50)
 # my_hash.set_item('lumber', 70)
 # my_hash.print_table()
+# Hash Table get method
+my_hash = HashTable()
+my_hash.set_item('bolts', 1400)
+my_hash.set_item('lumber', 70)
+print(my_hash.get_item('lumber'))
+print(my_hash.get_item('bolts'))
+print(my_hash.get_item('not there'))
